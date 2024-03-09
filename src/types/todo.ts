834a2f0eb83e6cee2ea75d06e640d97ef4388393
kimsohyeon <aotoyae@gmail.com>
2024-03-09 15:17:40 +0900

    Fix: map 타입 추가

--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,4 +1,7 @@
 export type Todos = {
+  map(
+    arg0: (todo: Todos) => import('react/jsx-runtime').JSX.Element
+  ): import('react').ReactNode;
   id: string;
   content: string;
   title: string;
